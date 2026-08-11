7e3563612ff25d80cab0e4bc62e057b94daf4091
EstherAuma <estheraumaego@gmail.com>
2026-08-11 04:29:20 +0300

    Add a modal

--- src/app/add-task/add-task.page.ts
+++ src/app/add-task/add-task.page.ts
@@ -1,20 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar,IonInput,IonItem,IonButton,IonCard,IonCardHeader,IonCardTitle,IonCardContent,IonCardSubtitle } from '@ionic/angular/standalone';
+import { 
+  IonContent, IonHeader, IonTitle, IonToolbar,
+  IonInput,IonItem,IonButton,IonCard,IonCardHeader,IonCardTitle,
+  IonCardContent,IonCardSubtitle,IonSelect,IonSelectOption,
+  IonDatetime,IonModal,IonText,IonLabel } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.page.html',
   styleUrls: ['./add-task.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonItem, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle, CommonModule, FormsModule, RouterLink]
+  imports: [
+    IonContent, IonHeader, IonTitle, IonToolbar, 
+    IonInput, IonItem, IonButton, IonCard, IonCardHeader, 
+    IonCardTitle, IonCardContent, IonCardSubtitle, IonSelect, 
+    IonSelectOption, IonDatetime, IonModal, IonText, IonLabel, CommonModule, FormsModule, RouterLink]
 })
 export class AddTaskPage implements OnInit {
+
+  isDatePickerOpen = false;
+  selectedDate = '';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openDatePicker() {
+    this.isDatePickerOpen = true;
+  }
+
+  closeDatePicker() {
+    this.isDatePickerOpen = false;
   }
 
 }
